f32a7a6bae6161658e700acc7917b1c220127d38
Colin Wallace <colin@mooooo.ooo>
2018-01-13 22:00:38 -0800

    Slow marquee name.
    Trying to go faster causes rate limiting. It seems that the slack api
    allows short bursts of sub-second calls, but doesn't like the average
    time between calls to be much less than about 7 seconds or so. This is
    dumb, but not much I can do about it. Their "Real Time Messaging" API
    appears to allow for editing user data despite the name, so I might try
    that.

---
+++ slackname.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from slackclient import SlackClient
+import csv, time
+
+# If an API call fails, how many seconds to wait before trying again
+rate_limit_sleep = 10
+
+token = open("api.token", "r").read().strip()
+client = SlackClient(token)
+
+
+def change_name_to(name):
+    while True: # yuck
+        resp = client.api_call("users.profile.set", profile=dict(display_name=name))
+        if resp.get('ok'):
+            break
+        print(resp)
+        time.sleep(rate_limit_sleep)
+
+
+while True:
+    for command in csv.reader(open("commands.csv")):
+        delay, name = command
+        delay = float(delay)
+        print("name = {name} for {delay} seconds".format(**globals()))
+        change_name_to(name)
+        time.sleep(delay)
+
