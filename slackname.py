73ddbfeecc438de925034ecc465b1dd846190fe3
Colin Wallace <colin@mooooo.ooo>
2019-03-21 14:28:58 -0700

    Update full name instead of display name
    Also, document the available fields & provide diagnostics on start.
    
    Note: not all orgs allow setting of the display name

--- slackname.py
+++ slackname.py
@@ -9,15 +9,28 @@ rate_limit_sleep = 60
 token = open("api.token", "r").read().strip()
 client = SlackClient(token)
 
-
 def change_name_to(name):
     while True: # yuck
-        resp = client.api_call("users.profile.set", profile=dict(display_name=name))
+        # some fields:
+        # display_name
+        # real_name
+        # first_name
+        # last_name
+        # image_512 (give it a URL)
+        # image_192 (URL)
+        # image_72 (URL)
+        # image_32 (URL)
+        # title
+        # skype
+        # email
+        # status_emoji
+        resp = client.api_call("users.profile.set", profile=dict(real_name=name))
         if resp.get('ok'):
             break
         print(resp)
         time.sleep(rate_limit_sleep)
 
+print(client.api_call('users.profile.get'))
 
 while True:
     for command in csv.reader(open(sys.argv[1])):
